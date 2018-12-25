f160ddf14453d1770274e504fc66f892e0004e5b
Dalton <phandb72@gmail.com>
2018-12-25 17:02:05 -0600

    changed to JDK 11

--- src/main/java/com/javaspringprojects/securityrestful/config/ApplicationConfig.java
+++ src/main/java/com/javaspringprojects/securityrestful/config/ApplicationConfig.java
@@ -9,136 +9,127 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
-@Configuration
-@EnableWebMvc
-@EnableTransactionManagement
-@ComponentScan(basePackages="com.javaspringprojects.securityrestful")
-@PropertySource({ "classpath:persistence-mysql.properties" })
 public class ApplicationConfig implements WebMvcConfigurer {
-
+	
 	// set up variable to hold the properties
-		@Autowired
-		private Environment env;
-		
-		// set up a logger for diagnostics
-		private Logger logger = Logger.getLogger(getClass().getName());
-		
-		/*  ***No Need viewResolver in REST**/
-		// define a bean for ViewResolver
-		@Bean
-		public ViewResolver viewResolver() {
+			@Autowired
+			private Environment env;
 			
-			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+			// set up a logger for diagnostics
+			private Logger logger = Logger.getLogger(getClass().getName());
 			
-			viewResolver.setPrefix("/WEB-INF/view/");
-			viewResolver.setSuffix(".jsp");
-			
-			return viewResolver;
-		}
-		
-		
-		// define a bean for our security datasource
-		
-		@Bean
-		public DataSource myDataSource() {
-			
-			// create connection pool
-			ComboPooledDataSource myDataSource = new ComboPooledDataSource();
-	 
-			// set the jdbc driver
-			try {
-				myDataSource.setDriverClass("com.mysql.jdbc.Driver");		
-			}
-			catch (PropertyVetoException exc) {
-				throw new RuntimeException(exc);
+			/*  ***No Need viewResolver in REST**/
+			// define a bean for ViewResolver
+			@Bean
+			public ViewResolver viewResolver() {
+				
+				InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+				
+				viewResolver.setPrefix("/WEB-INF/view/");
+				viewResolver.setSuffix(".jsp");
+				
+				return viewResolver;
 			}
 			
-			// for sanity's sake, let's log url and user ... just to make sure we are reading the data
-			logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
-			logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
 			
-			// set database connection props
-			myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
-			myDataSource.setUser(env.getProperty("jdbc.user"));
-			myDataSource.setPassword(env.getProperty("jdbc.password"));
+			// define a bean for our security datasource
 			
-			// set connection pool props
-			myDataSource.setInitialPoolSize(
-			getIntProperty("connection.pool.initialPoolSize"));
-	 
-			myDataSource.setMinPoolSize(
-					getIntProperty("connection.pool.minPoolSize"));
+			@Bean
+			public DataSource myDataSource() {
+				
+				// create connection pool
+				ComboPooledDataSource myDataSource = new ComboPooledDataSource();
+		 
+				// set the jdbc driver
+				try {
+					myDataSource.setDriverClass("com.mysql.jdbc.Driver");		
+				}
+				catch (PropertyVetoException exc) {
+					throw new RuntimeException(exc);
+				}
+				
+				// for sanity's sake, let's log url and user ... just to make sure we are reading the data
+				logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
+				logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
+				
+				// set database connection props
+				myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+				myDataSource.setUser(env.getProperty("jdbc.user"));
+				myDataSource.setPassword(env.getProperty("jdbc.password"));
+				
+				// set connection pool props
+				myDataSource.setInitialPoolSize(
+				getIntProperty("connection.pool.initialPoolSize"));
+		 
+				myDataSource.setMinPoolSize(
+						getIntProperty("connection.pool.minPoolSize"));
+				
+				myDataSource.setMaxPoolSize(
+						getIntProperty("connection.pool.maxPoolSize"));
+				
+				myDataSource.setMaxIdleTime(
+						getIntProperty("connection.pool.maxIdleTime"));
+						
+				return myDataSource;
+			}
 			
-			myDataSource.setMaxPoolSize(
-					getIntProperty("connection.pool.maxPoolSize"));
+			// need a helper method 
+			// read environment property and convert to int
 			
-			myDataSource.setMaxIdleTime(
-					getIntProperty("connection.pool.maxIdleTime"));
-					
-			return myDataSource;
-		}
-		
-		// need a helper method 
-		// read environment property and convert to int
-		
-		private int getIntProperty(String propName) {
+			private int getIntProperty(String propName) {
+				
+				String propVal = env.getProperty(propName);
+				
+				// now convert to int
+				int intPropVal = Integer.parseInt(propVal);
+				
+				return intPropVal;
+			}
 			
-			String propVal = env.getProperty(propName);
+			private Properties getHibernateProperties() {
+		 
+				// set hibernate properties
+				Properties props = new Properties();
+		 
+				props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+				props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
 			
-			// now convert to int
-			int intPropVal = Integer.parseInt(propVal);
+				return props;				
+			}
+		 
 			
-			return intPropVal;
-		}
-		
-		private Properties getHibernateProperties() {
-	 
-			// set hibernate properties
-			Properties props = new Properties();
-	 
-			props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-			props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-		
-			return props;				
-		}
-	 
-		
-		@Bean
-		public LocalSessionFactoryBean sessionFactory(){
+			@Bean
+			public LocalSessionFactoryBean sessionFactory(){
+				
+				// create session factory
+				LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+				
+				// set the properties
+				sessionFactory.setDataSource(myDataSource());
+				sessionFactory.setPackagesToScan(env.getProperty("hiberante.packagesToScan"));
+				sessionFactory.setHibernateProperties(getHibernateProperties());
+				
+				return sessionFactory;
+			}
 			
-			// create session factory
-			LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-			
-			// set the properties
-			sessionFactory.setDataSource(myDataSource());
-			sessionFactory.setPackagesToScan(env.getProperty("hiberante.packagesToScan"));
-			sessionFactory.setHibernateProperties(getHibernateProperties());
-			
-			return sessionFactory;
-		}
-		
-		@Bean
-		@Autowired
-		public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-			
-			// setup transaction manager based on session factory
-			HibernateTransactionManager txManager = new HibernateTransactionManager();
-			txManager.setSessionFactory(sessionFactory);
-	 
-			return txManager;
-		}	
+			@Bean
+			@Autowired
+			public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+				
+				// setup transaction manager based on session factory
+				HibernateTransactionManager txManager = new HibernateTransactionManager();
+				txManager.setSessionFactory(sessionFactory);
+		 
+				return txManager;
+			}	
+
 }
